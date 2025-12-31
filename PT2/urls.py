a77cd2468a7b5350cbe5ea2c1fa6447b478efdc9
karla22rios <karlad.rios02@gmail.com>
2025-12-31 11:30:42 -0600

    Fix: servir media en producción

--- PT2/urls.py
+++ PT2/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework.routers import DefaultRouter
 from viewer.api import CategoryView, LessonView, StepView, AssetView
 
@@ -12,10 +12,10 @@ router.register(r"api/steps", StepView, basename="api-steps")
 router.register(r"api/assets", AssetView, basename="api-assets")
 
 urlpatterns = [
+   re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     path("admin/", admin.site.urls),
     path("", include("viewer.urls")),
     path("", include(router.urls)),
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
